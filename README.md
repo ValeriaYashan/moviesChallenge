80dcdb9596576017055122af8525f661f4f3ec31
Valeriayashan <valeriayashan@gmail.com>
2022-02-09 12:51:13 -0300

    en readme avances-esto es prueba

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # moviesChallenge
+
+##commit 1##
+-Creación de repositorio en Github "Movies Challenge"
+-Creación de proyecto en Node con Movies_db
+-Creación de rutas, controladores y vistas ( EJS )
+
+##commit 2##
+-Creación del Home-page del proyecto, debe mostrar el listado de todas las películas
+
+FALTA VISTA QUE SOLO MUESTRE EL LISTADO SIN EL CRUD-PARA USUARIOS
+
+##commit 3##
+-Cada película tendrá un link que muestre el detalle completo. En el detalle deben verse los datos más el nombre del género al que pertenece y la lista de actores que trabajaron en ella.
+(Implementar sequelize, crear modelos y establecer relaciones)
+
+FALTA LA LISTA DE ACTORES
+FALTA QUE SI ESTA VACIO ALGUN CAMPO ENTONCES NO LO MUESTRE
+
+
+##commit 4##
+CRUD
+-Agregar una nueva película, modificarla, eliminarla (paranoid)
+-Colocar validaciones para los campos de formularios que son obligatorios
+
+AGREGAR MAS CAMPOS
+ARREGLAR NO FUNCIONAN BOTONES MODIFICAR, MEJORAR LA PREGUNTA DEL DELETE CON VENTANA
+##commit 5##
+
+##commit 6##
+
+##commit 7##
+-Mejoro estilos y responsive
--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -12,6 +12,7 @@ const Actors = db.Actor;
 
 
 const moviesController = {
+ 
     'list': (req, res) => {
         db.Movie.findAll({
             include: ['genre']
@@ -29,31 +30,7 @@ const moviesController = {
                 res.render('moviesDetail.ejs', {movie});
             });
     },
-    'new': (req, res) => {
-        db.Movie.findAll({
-            order : [
-                ['release_date', 'DESC']
-            ],
-            limit: 5
-        })
-            .then(movies => {
-                res.render('newestMovies', {movies});
-            });
-    },
-    'recomended': (req, res) => {
-        db.Movie.findAll({
-            include: ['genre'],
-            where: {
-                rating: {[db.Sequelize.Op.gte] : 8}
-            },
-            order: [
-                ['rating', 'DESC']
-            ]
-        })
-            .then(movies => {
-                res.render('recommendedMovies.ejs', {movies});
-            });
-    },
+   
     //Aqui dispongo las rutas para trabajar con el CRUD
     add: function (req, res) {
         let promGenres = Genres.findAll();
