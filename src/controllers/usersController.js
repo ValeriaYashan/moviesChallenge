0455d0a513f9134dd2fbfe0038370d831022e729
Valeriayashan <valeriayashan@gmail.com>
2022-02-15 16:02:11 -0300

    rutas completas

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 const db = require("./../database/models");
 const allUsers = db.User;
 const sequelize = db.sequelize;
-const crypto = require("crypto");
+
 
 const usersController = {
 
--- src/middlewares/logmiddleware.js
+++ src/middlewares/logmiddleware.js
@@ -2,7 +2,7 @@ function logMiddleware(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.redirect('/user/login');
+        res.redirect('/users/login');
     }
 }
 
